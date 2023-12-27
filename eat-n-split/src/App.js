128b723b0445d25d7f6581fe803448c7ccaab47c
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2023-12-27 23:23:41 +0200

    Update App.js

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -76,6 +76,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
